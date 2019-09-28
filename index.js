38093482eebdd1295d5429b8cb13f0559532e255
unknown <abhishekharshvardhan@gmail.com>
2019-09-28 15:45:10 +0530

    config added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hi there' });
-});
+const app = express();
+app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.coockieKey] }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./services/passport');
+require('./routes/authRoute')(app);
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(keys.mongoURI, options);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
