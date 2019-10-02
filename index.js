5e32e1f57e68aeaa5413aced81c25af4a5540d02
unknown <abhishekharshvardhan@gmail.com>
2019-10-02 15:39:19 +0530

    gi

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ require('./services/passport');
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-// if (process.env.NODE_ENV === 'production') {
-app.use(express.static('client/build'));
-const path = require('path');
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose.connect(keys.mongoURI, options);
